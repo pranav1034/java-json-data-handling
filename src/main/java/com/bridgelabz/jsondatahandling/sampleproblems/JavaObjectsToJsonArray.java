8770cf0fa6d72d9832b2a2008344466e404ee9e2
pranav1034 <pranavaggarwal04@gmail.com>
2025-04-10 14:13:40 +0530

    [Pranav] ADD convert-list-of-java-objrcts-into-json-array

---
+++ src/main/java/com/bridgelabz/jsondatahandling/sampleproblems/JavaObjectsToJsonArray.java
@@ -0,0 +1,46 @@
+package com.bridgelabz.jsondatahandling.sampleproblems;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.JsonArray;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Person {
+    private String name;
+    private int age;
+    private String city;
+
+    public Person(){
+    }
+    public Person(String name, int age, String city) {
+        this.name = name;
+        this.age = age;
+        this.city = city;
+    }
+
+    public String getName() { return name; }
+    public int getAge() { return age; }
+    public String getCity() { return city; }
+}
+
+public class JavaObjectsToJsonArray {
+    public static void main(String[] args) {
+        Person person1 = new Person("Pranav", 22, "Chandigarh");
+        Person person2 = new Person("Nikhil", 21, "Patiala");
+
+        List<Person> peopleList = new ArrayList<>();
+        peopleList.add(person1);
+        peopleList.add(person2);
+
+        ObjectMapper mapper = new ObjectMapper();
+        try{
+            String jsonArray = mapper.writeValueAsString(peopleList);
+            System.out.println(jsonArray);
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+
+    }
+}
